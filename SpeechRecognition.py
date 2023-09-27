ded0c7b3f18982a3349a4a7e979791c534a974de
Emy Pio <89119758+EmyPio@users.noreply.github.com>
2023-09-27 12:42:56 +0530

    Update SpeechRecognition.py
    extracting text from wav format files

--- SpeechRecognition.py
+++ SpeechRecognition.py
@@ -18,3 +18,43 @@ for ogg_file in ogg_files:
     ogg_audio.export(wav_file, format="wav")
 
     print(f"Converted {ogg_file} to {wav_file}")
+
+# Initialize the recognizer
+recognizer = sr.Recognizer()
+
+# List of WAV audio file names
+wav_files = ["audiotest.wav", "audiotest1.wav", "audiotest2.wav"]  # Add more file names as needed
+
+# Create a folder to store the transcriptions
+if not os.path.exists('transcriptions'):
+    os.mkdir('transcriptions')
+
+# Initialize a counter for naming the transcription files
+file_counter = 1
+
+# Loop through each WAV audio file and transcribe it
+for wav_file in wav_files:
+    try:
+        # Load the WAV audio file
+        with sr.AudioFile(wav_file) as source:
+            audio_data = recognizer.record(source)  # Record the audio data
+
+        # Recognize the speech
+        text = recognizer.recognize_google(audio_data)
+
+        # Write the transcription to a text file
+        transcription_file = f'transcriptions/transcription{file_counter}.txt'
+        with open(transcription_file, 'w') as transcribed_text:
+            transcribed_text.write(f"Audio File: {wav_file}\n")
+            transcribed_text.write(f"Transcribed Text: {text}\n")
+
+        print(f"Transcription for {wav_file}:\n{text}\n")
+
+        file_counter += 1
+    except sr.UnknownValueError:
+        print(f"Could not understand the audio in {wav_file}\n")
+    except sr.RequestError as e:
+        print(f"Could not request results for {wav_file}; {e}\n")
+
+print("Transcription process completed.")
+
