d0a827e64b224d5b9c46725f9ef5f11a95848fb1
Emy Pio <89119758+EmyPio@users.noreply.github.com>
2023-09-27 14:31:26 +0530

    Create sentiment_analysis .py
    sentiment anaysis of audio text

---
+++ sentiment_analysis .py
@@ -0,0 +1,18 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from string import punctuation
+
+nltk.download("stopwords")
+nltk.download("punkt")
+
+stop_words = set(stopwords.words("english"))
+
+def preprocess_text(text):
+    text = text.lower()  # Convert to lowercase
+    text = "".join([char for char in text if char not in punctuation])  # Remove punctuation
+    words = word_tokenize(text)  # Tokenize the text
+    words = [word for word in words if word not in stop_words]  # Remove stopwords
+    return " ".join(words)
+
+df["Processed Text"] = df["Transcribed Text"].apply(preprocess_text)
